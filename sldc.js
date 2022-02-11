09723046e01f9fea5e77048252e1823ffd102cbb
TYama <alohaty81@gmail.com>
2022-02-11 10:22:38 -0700

    sldc.js is done

--- sldc.js
+++ sldc.js
@@ -5,17 +5,22 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Analysis of Requirements
-// Design
-// Implementation
-// Maintain
-// Planning
-// Testing & Integration
-
+// 1. Planning
+// 2. Analysis of Requirements
+// 3. Design
+// 4. Implementation
+// 5. Testing & Integration
+// 6. Maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
-    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    Create a variable for each of the above steps of the Software Development Lifecycle and 
+    set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "In this stage, the team members gather all the specific details required for a new system, and dtermin the first ideas for prototypes."
+const design = "The designing stage, developers outline the details for the overall application, and also design/document the operation, training, and maintenance plans so that the team members know what they need to do throughout every stage of the SDLC."
+const implementation = "In implementation stage, develpers actually write code and build the application according to the design plans/documents that were outlined in the previous stage."
+const testingAndIntegration = "In this stage, the application has to be tested to make sure that there are not any bugs and it is developed as designed. After testing, the overall design for the software will be integrated together to be ready for the end-users. "
+const maintain = "After the application started being used by the end-users, developers make effort to maintain the application through making nessesary changes to the application according to the end-user report and etc."
